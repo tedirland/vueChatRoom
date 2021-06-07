aac31921b837d72fac4fa014e1d7f12468be7126
Ted Irland <irlandth@gmail.com>
2021-06-07 13:46:44 -0500

    Created a loginform and signupform component and nested inside the welcome.vue component

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <SignupForm />
+    <!-- <LoginForm /> -->
   </div>
 </template>
 
-<style>
-.welcome {
-  text-align: center;
-  padding: 20px 0;
+<script>
+import SignupForm  from '../components/SignupForm.vue'
+import LoginForm  from '../components/LoginForm.vue'
+
+export default {
+  components: {SignupForm,LoginForm},
+  setup() {
+
+  },
 }
+</script>
+
+<style>
+ .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
 </style>
