9827e8f5df10a03b4fe41dcd88ecd1c24867e469
Ted Irland <irlandth@gmail.com>
2021-06-07 13:31:58 -0500

    created new firestore backend and set it up in a new firebase folder/ config.js file

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDeuaUZmIdhSTdqIvc9rhqGen41D9C9mu8',
+  authDomain: 'dojo-blog-86ab1.firebaseapp.com',
+  projectId: 'dojo-blog-86ab1',
+  storageBucket: 'dojo-blog-86ab1.appspot.com',
+  messagingSenderId: '823471080492',
+  appId: '1:823471080492:web:aaeecfedb45691031afd89',
+};
+
+//init firebase
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
