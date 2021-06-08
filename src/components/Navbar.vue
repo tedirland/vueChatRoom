739c2e99cc75b41b88b848e393612d86ee5c5229
Ted Irland <irlandth@gmail.com>
2021-06-08 07:41:58 -0500

    Created Navbar component and styled appropriately. Nested inside welcome.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey There... display name here</p>
+      <p class="email">Currently logged in as ...</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
