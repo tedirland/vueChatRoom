54a183626c3a7f562d57d19fb3e1176c2a2af17e
Ted Irland <irlandth@gmail.com>
2021-06-08 18:21:26 -0500

    styled chat form and console logged result when the user enters a message

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import NewChatForm from '../components/NewChatForm.vue'
+
 import getUser from '../composables/getUser'
 import {useRouter} from 'vue-router'
-import { projectAuth } from '../firebase/config';
 import { watch } from '@vue/runtime-core';
 
 export default {
-components: {Navbar},
+components: {Navbar, NewChatForm},
 setup() {
   const router = useRouter()
   const {user} = getUser()
