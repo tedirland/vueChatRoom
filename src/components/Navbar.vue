9f2e4cfb1e724e152486f47c3030b7a6441f2332
Ted Irland <irlandth@gmail.com>
2021-06-08 07:48:42 -0500

    Created a composable to log the user out and hooked it up to the button in the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,12 +4,28 @@
       <p>Hey There... display name here</p>
       <p class="email">Currently logged in as ...</p>
     </div>
-    <button>Logout</button>
+    <button @click="handleClick">Logout</button>
   </nav>
 </template>
 
 <script>
+import useLogout from '../composables/useLogout'
 export default {
+  setup() {
+    const {logout, error} = useLogout()
+
+    const handleClick = async () => {
+
+      await logout()
+      if(!error.value) {
+        console.log('user logged out')
+      }
+
+    }
+
+    return {handleClick, error}
+  }
+
 
 }
 </script>
