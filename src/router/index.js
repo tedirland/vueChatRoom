507aca73f1ed6d52f1e17363c6da3081b26266ef
Ted Irland <irlandth@gmail.com>
2021-06-09 08:54:05 -0500

    Created a route guard to 'protect' the root route from being visited by users who are currently logged in

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,24 @@ import { projectAuth } from '../firebase/config';
 
 const requireAuth = (to, from, next) => {
   let user = projectAuth.currentUser;
-  console.log('current user in auth guard:', user);
   if (!user) {
     next({ name: 'Welcome' });
   } else next();
 };
 
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+
+  if (user) {
+    next({ name: 'Chatroom' });
+  } else next();
+};
 const routes = [
   {
     path: '/',
     name: 'Welcome',
     component: Welcome,
+    beforeEnter: requireNoAuth,
   },
   {
     path: '/chatroom',
